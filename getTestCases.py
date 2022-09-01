24c16fbd8d48de7e9cfea1e355ff73ee857b6dcd
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2022-09-01 16:41:52 +0200

    Update

--- getTestCases.py
+++ getTestCases.py
@@ -3,6 +3,7 @@
 from urllib.request import urlopen
 from sys import argv
 from html.parser import HTMLParser
+import requests
 
 # This parse break should be the same in your run file
 PARSE_BREAK = "BREAK PARSE HERE\n"
@@ -14,13 +15,26 @@ class problem_parser(HTMLParser):
     self.input = []
     self.output = []
     self.buffer = ""
+    self.atTests = False
+    self.level = 0
 
   def handle_starttag(self, tag, attrs):
-    if tag == "pre":
+    if tag == 'div' and ('class', 'sample-tests') in attrs:
+      self.atTests = True
+
+    if self.atTests:
+      self.level += 1
+
+    if tag == "pre" and self.atTests:
       self.reading = True
 
   def handle_endtag(self, tag):
-    if tag == "pre":
+    if self.atTests:
+      self.level -= 1
+      if self.level == 0:
+        self.atTests = False
+
+    if tag == "pre" and self.atTests:
       self.reading = False
       if len(self.input) == len(self.output):
         self.input.append(self.buffer)
@@ -30,23 +44,29 @@ class problem_parser(HTMLParser):
 
   def handle_data(self, data):
     if self.reading:
-      self.buffer += data + '\n'
+      self.buffer += data.strip() + '\n'
 
 contest_number = argv[1]
+response = requests.get('http://www.codeforces.com/api/contest.standings', params={'contestId': contest_number, 'handles': 'Goldsmith94'})
 
-problem_letter = 'A'
-while True:
+response.raise_for_status()
+problem_names = map(lambda x: x['index'], response.json()['result']['problems'])
+for problem_letter in problem_names:
+  print("Fetching problem", problem_letter)
   url = 'http://codeforces.com/contest/' + contest_number + '/problem/' + problem_letter
   problem = urlopen(url)
   if problem.geturl() != url:
+    print("Testcases not found")
     break
   else:
-    print("Fetching problem", problem_letter)
+    print("Fetch successful")
     text = problem.read()
     parser = problem_parser()
     parser.feed(text.decode('utf-8'))
     if len(parser.input) != len(parser.output):
       print("Error with problem", problem_letter)
+      print(parser.input)
+      print(parser.output)
     else:
       inp = open(problem_letter + '.txt', 'w')
       inp.write(str(len(parser.input)) + '\n')
@@ -60,6 +80,3 @@ while True:
         out.write(test_case)
         out.write(PARSE_BREAK)
       out.close()
-  print("Fetch successful")
-  problem_letter = chr(ord(problem_letter)+1)
-
--- run.py
+++ run.py
@@ -1,48 +1,64 @@
 #!/usr/bin/python3
 from sys import argv
-from subprocess import run, PIPE
+from subprocess import STDOUT, run, PIPE
 from time import time
+import json
 
 # This parse break should be the same in your get test cases file
 PARSE_BREAK = "BREAK PARSE HERE\n"
 
 problem_letter = argv[1].lower()
 check_output = len(argv) > 2 and int(argv[2])
-inp = open(problem_letter.upper() + '.txt', 'r')
-out = open(problem_letter.upper() + '.out', 'r')
+inp = open(problem_letter.upper() + ".txt", "r")
+out = open(problem_letter.upper() + ".out", "r")
 num_cases = int(inp.readline())
 if int(out.readline()) != num_cases and check_output:
-  print("Error: Number of test cases in input and answer files are different")
-  exit()
+    print("Error: Number of test cases in input and answer files are different")
+    exit()
 for i in range(1, num_cases + 1):
-  case_input = ""
-  line = inp.readline()
-  while line != PARSE_BREAK:
-    case_input += line
+    case_input = ""
     line = inp.readline()
-  start_time = time()
-  output = run(["./" + problem_letter + ".exe"],
-    input=case_input.encode('utf-8'), stdout=PIPE).stdout.decode('utf-8')
-  time_taken = time()-start_time
-  if check_output:
-    case_output = ""
-    line = out.readline()
     while line != PARSE_BREAK:
-      case_output += line
-      line = out.readline()
-    print("Case #{}: {}".format(i, case_output == output))
-    print("Time taken: {}".format(time_taken))
-    if case_output != output:
-      print("Input:")
-      print(case_input, end='')
-      print("Your output:")
-      print(output, end='')
-      print("Correct output:")
-      print(case_output, end='')
-  else:
-    print("Case #{}:".format(i))
-    print(output, end='')
-    print("Time taken: {}".format(time_taken))
-  print()
+        case_input += line
+        line = inp.readline()
+    start_time = time()
+    output = run(
+        ["./" + problem_letter[0].lower() + ".exe"],
+        input=case_input.encode("utf-8"),
+        stdout=PIPE,
+        stderr=PIPE,
+    )
+    time_taken = time() - start_time
+    if check_output:
+        case_output = ""
+        line = out.readline()
+        while line != PARSE_BREAK:
+            case_output += line.rstrip() + "\n"
+            line = out.readline()
+        if output.stdout.decode("utf-8")[-1] != "\n":
+            print("OUTPUT MUST END IN NEWLINE")
+            break
+        processed_actual_output = (
+            "\n".join(
+                map(lambda s: s.rstrip(), output.stdout.decode("utf-8").splitlines())
+            )
+            + "\n"
+        )
+        case_was_correct = case_output == processed_actual_output
+        print("Case #{}: {}".format(i, case_was_correct))
+        print("Time taken: {}".format(time_taken))
+        if not case_was_correct:
+            print("Input:")
+            print(case_input, end="")
+            print("Your stderr:")
+            print(output.stderr.decode("utf-8"), end="")
+            print("Your output:")
+            print(processed_actual_output, end="")
+            print("Correct output:")
+            print(case_output, end="")
+    else:
+        print("Case #{}:".format(i))
+        print(output, end="")
+        print("Time taken: {}".format(time_taken))
 inp.close()
 out.close()
